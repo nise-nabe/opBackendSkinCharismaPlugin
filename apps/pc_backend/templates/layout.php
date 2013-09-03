54ded5cb480b06c3d24137b3fa2f1e1232941191
watanabe <watanabe@openpne.jp>
2013-09-03 10:37:51 +0900

    add feature to change theme

--- apps/pc_backend/templates/layout.php
+++ apps/pc_backend/templates/layout.php
@@ -57,6 +57,25 @@
           <li><a href="<?php echo app_url_for('pc_frontend', '@homepage', true) ?>" target="_blank"><?php echo __('Visit Site') ?></a></li>
         </ul>
       </div>
+      <!-- theme selector starts -->
+      <div class="btn-group pull-right theme-container" >
+        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+          <i class="icon-tint"></i><span class="hidden-phone"> Change Theme / Skin</span>
+          <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu" id="themes">
+          <li><a data-value="classic" href="#"><i class="icon-blank"></i> Classic</a></li>
+          <li><a data-value="cerulean" href="#"><i class="icon-blank"></i> Cerulean</a></li>
+          <li><a data-value="cyborg" href="#"><i class="icon-blank"></i> Cyborg</a></li>
+          <li><a data-value="redy" href="#"><i class="icon-blank"></i> Redy</a></li>
+          <li><a data-value="journal" href="#"><i class="icon-blank"></i> Journal</a></li>
+          <li><a data-value="simplex" href="#"><i class="icon-blank"></i> Simplex</a></li>
+          <li><a data-value="slate" href="#"><i class="icon-blank"></i> Slate</a></li>
+          <li><a data-value="spacelab" href="#"><i class="icon-blank"></i> Spacelab</a></li>
+          <li><a data-value="united" href="#"><i class="icon-blank"></i> United</a></li>
+        </ul>
+      </div>
+      <!-- theme selector ends -->
     </div>
   </div>
 </div>
